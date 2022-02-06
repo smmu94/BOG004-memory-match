c7d054e92bf8d81044d3aa02616f55339ecddc24
smmu94 <stephaniemmu@gmail.com>
2022-02-05 21:52:50 -0500

    cambios

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@
 // Para incluir los diferentes sets de cartas podemos _importar_ el archivo
 // JavasSript que contenga el `export` correspondiente...
 //
- import marvel from '../data/marvel/marvel.js';
- //console.log(marvel);
+//import marvel from '../data/marvel/marvel.js';
+//console.log(marvel);
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
@@ -14,62 +14,48 @@
 //   .catch(console.error);
 //
 
-/*fetch('https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=iro&ts=1643905564948&apikey=d43933949ba7e8943bf5902021f17f7e&hash=e02a592e20d9ea7408db96cb8677f0ff')
+fetch('../data/marvel/marvel.json')
   .then(resp => resp.json())
   .then(data => App(data))
-  .catch(err => console.error(err));*/
-
-
-
-/*const App = (marvel) => {
+  .catch(err => console.error(err))
   
-  const el = document.createElement('div');
-
-  el.className = 'App';
-  el.innerHTML = marvel.item[0];
-
+const App = (data) => {
+  
+let images = data.items.map(function(x) {
+  const el = document.createElement('img');
+  el.setAttribute("src",x.image);
+  el.id = "" + x.id;
   return el;
-};*/
-const App = () => {
-
+ })
   
-
-   
-    let imagen = marvel.items.map(function(el) {
-      el = document.createElement('img');
-      el = marvel.items.image;
-      return el;
-   });
-  
-    /*for (let i = 0; i < marvel.items.length; i++) {
-    el = document.createElement('img');
-    el.setAttribute("src",marvel.items[i].image);
-    el.className = 'App';
-    imagen[i] = el;
-    console.log(imagen[i]);
-    
-  } */
-
-
-
-   
-  return imagen.src;
-    /*const el1 = document.createElement('img');
-    el1.setAttribute("src",marvel.items[0].image);
-    el1.className = 'App';
-    console.log(el1);
-
-    const el2 = document.createElement('img');
-    el2.setAttribute("src",marvel.items[1].image);
-    el2.className = 'App';
-    console.log(el2);*/
-
+return console.log(images[0],images[1]); }
   
   
-  //document.getElementsByClassName("App").appendChild("hulk");
 
   
+ /* const images = data.items.map 
+  data.items.forEach(function(element){
+    const el = document.createElement('img');
+    el.setAttribute("src", element.image);
+    el.id = "" + element.id;
+    return el;
+  }
+  
+  const App = (data) => {
 
-}
+  let image = new Array(data.items.length-1);
+  
+  for (let i = 0; i < data.items.length; i++)
+  {
+    const el = document.createElement('img');
+    el.setAttribute("src", data.items[i].image);
+    el.id = "" + data.items[i].id;
+    image[i] = el;
+    console.log(image[i]);
+  }
+  
+  return image;
 
+}  */
+  
 export default App;
